2a0d5e71800b4c69513a170d26a805d6e0642095
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-08-19 14:21:07 +0200

    Add navigation layout

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,21 +1,30 @@
 export function Calendar() {
   return (
-    <>
+    <div className="border border-red-500 rounded-md max-w-[1500px] m-auto">
       <Navigation />
       <Month />
       <AddEventModal />
       <EventDetailsModal />
       <ViewMoreModal />
-    </>
+    </div>
   );
 }
 
 function Navigation() {
-  return <button>Today</button>;
+  return (
+    <nav className="flex items-center gap-4 my-4 ml-4">
+      <button className="px-4 py-1 border rounded-md hover:bg-gray-400">
+        Today
+      </button>
+      <button>&lt;</button>
+      <button>&gt;</button>
+      <span className="font-semibold">August 2024</span>
+    </nav>
+  );
 }
 
 function Month() {
-  return <h1>August</h1>;
+  return <main className="border rounded-md max-w-[1500px]">Days Grid</main>;
 }
 
 function AddEventModal() {
