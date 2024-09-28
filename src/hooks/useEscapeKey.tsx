6a0995e088e89fbcfd873cd21dfb0cce71ef701c
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-09-28 06:59:59 +0200

    Fix bug by adding a stack to track open modals

--- src/hooks/useEscapeKey.tsx
+++ src/hooks/useEscapeKey.tsx
@@ -1,10 +1,17 @@
 import { useEffect } from "react";
 
+const modalStack: (() => void)[] = [];
+
 export function useEscapeKey(onClose: () => void) {
   useEffect(() => {
+    modalStack.push(onClose); // Register this modal as the most recent one
+
     function handleKeyDown(event: KeyboardEvent) {
-      if (event.key === "Escape") {
-        onClose();
+      if (
+        event.key === "Escape" &&
+        modalStack[modalStack.length - 1] === onClose
+      ) {
+        onClose(); // Only close the top-most modal
       }
     }
 
@@ -12,6 +19,11 @@ export function useEscapeKey(onClose: () => void) {
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
+      // Remove modal from the stack on unmount
+      const index = modalStack.indexOf(onClose);
+      if (index > -1) {
+        modalStack.splice(index, 1);
+      }
     };
   }, [onClose]);
 }
